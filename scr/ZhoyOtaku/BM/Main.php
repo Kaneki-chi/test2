28141803c1032d4df15ea4476a2cad0707be42ea
Kaneki-chi <30436794+Kaneki-chi@users.noreply.github.com>
2017-11-13 20:10:59 -0500

    Update Main.php

--- scr/ZhoyOtaku/BM/Main.php
+++ scr/ZhoyOtaku/BM/Main.php
@@ -1,1 +1,18 @@
-namespace BanManager
+namespace BanManager;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmineentity\Entity;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommmandSender;
+use pocketmine\command\Command;
+use pocketmine\even\Listener;
+use pocketmone\event\PlayerQuitEvent;
+use pocketmine\event\PlayerJoinEvent;
+use pocketmine\event\PlayerPreLogin;
+use pocketmine\utils\TextFormat as TE;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+ public function onEnable():
+ $this->getServer()->getPluginManager()->registerEvents($this, $this);
